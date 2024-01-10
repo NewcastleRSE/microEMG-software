bc6a32f7aa07a3bd0a5d31d3854f0724ef3c7a3f
Gabrielle M. Schroeder <8205451+gmschroe@users.noreply.github.com>
2024-01-10 11:39:01 +0000

    moved classes EMGChannels and EMGPxx from emg_recordings module to improve organisation

--- analysis_software/pymicroemg/pymicroemg/emg_recording.py
+++ analysis_software/pymicroemg/pymicroemg/emg_recording.py
@@ -6,7 +6,7 @@ A set of classes for representing EMG recordings
 @author: Gabrielle
 """
 # TODO: add class, method docstrings (see numpy, google, pep8 styles)
-# TODO: consider making each class a separate file
+
 from __future__ import annotations
 
 import os # see pathlib as alternative for
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 import scipy.signal
 from typing import Union
 
+from pymicroemg.emg_channels import EMGChannels
+from pymicroemg.emg_pxx import EMGPxx
 
 class EMGData:
     '''
@@ -486,75 +488,6 @@ class EMGData:
         # analysis; also allows multiple PSDs to be created at different
         # preprocessing steps.
         return emg_pxx
-
-
-class EMGChannels:
-    '''
-    Class for representing EMG recording channels.
-    
-    attributes to add:
-    low quality channels (automatic detection)
-    low quality channels (visual inspection)
-    
-    '''
-    
-    def __init__(self, intan_chan_names: list[str]):
-        '''
-        Initialise EMGChannels object for modelling EMG channels.
-
-        Parameters
-        ----------
-        chan_names : list[str]
-            List of channel names.
-
-        Returns
-        -------
-        None.
-
-        '''
-        # original channel names
-        self.intan_chan_names = intan_chan_names 
-        
-        # Create new channel labels using the channel order 
-        # (i.e., label channels from 1 to n channels)
-        self.chan_names = ['channel ' + str(i) 
-                           for i in np.arange(1, len(intan_chan_names) + 1)]
-        
-        # x, y coordinates
-        self._get_chan_xy()                      
-
-    def _get_chan_xy(self):
-        '''
-        Get the channel (x, y) coordinates based on the number of channels, 
-        which determines the electrode's design.
-
-        Returns
-        -------
-        None.
-
-        '''
-
-        # Channel layout (in mm)
-        CHAN_SPACING_X = 0.3    # spacing along length (defined as x axis)
-        CHAN_SPACING_Y = 0.36   # spacing along width (defined as y axis)
-        CHAN_SHIFT_X = 0.3      # value used to shift x axis positions
-        
-        n_chan = len(self.chan_names)
-        
-        # First column will be x coordinates (position along length)
-        # Second column will be y coordinates (position along width)
-        self.chan_xy = np.zeros((n_chan, 2))
-
-        # Set x - evenly spaced, and shifted by CHAN_SHIFT_X
-        self.chan_xy[:,0] = np.arange(
-            CHAN_SPACING_X, 
-            CHAN_SPACING_X*(n_chan+1), 
-            CHAN_SPACING_X
-            ) + CHAN_SHIFT_X
-
-        # Set y - alternating positive and negative to form zig-zag
-        self.chan_xy[:,1] = CHAN_SPACING_Y/2
-        self.chan_xy[np.arange(1,n_chan+1,2),1] *= -1
         
         
 class EMGPreprocSettings:
@@ -580,116 +513,5 @@ class EMGPreprocSettings:
             self.filter_settings = {}
         else:
             self.filter_settings = filter_settings
-            
-        
-class EMGPxx:
-    '''
-    Class for representing power spectral density (PSD) computed from EMG 
-    recording.
-    '''
-    
-    def __init__(self, freq: npt.NDArray[np.float64], 
-                 pxx: npt.NDArray[np.float64], chan: EMGChannels, 
-                 window_size: float):
-        '''
-        Initialise EMGPxx object for modelling EMG's power spectral density 
-        (PSD).
-
-        Parameters
-        ----------
-        freq : 1D npt.NDArray[np.float64]
-            Vector of PSD frequencies.
-        pxx : 2D npt.NDArray[np.float64]
-            PSD of each channel's signal. Each row corresponds to one channel. 
-            Columns correspond to the frequencies in freq.
-        chan : EMGChannels
-            EMGChannels object for the corresponding EMG channels.
-        window_size : float
-            Size of window used to compute the PSD using Welch's method.
-
-        Returns
-        -------
-        None.
-
-        '''
-        self.freq = freq
-        self.pxx = pxx
-        self.chan = chan
-        self.window_size = window_size
-        self.n_chan = len(self.chan.chan_names)
-        
-    def plot_pxx(self, start_freq, stop_freq, ax=None, plot_chan=None,
-                 figsize=(5,5), lw=0.5):
-        '''
-        Plot the power spectral density (PSD) of one or all channels. If all
-        channels' PSDs are plotted, plots will be overlaid in one figure.
-
-        Parameters
-        ----------
-        start_freq : float
-            Beginning of the frequency range to plot.
-        stop_freq : float
-            End of the frequency range to plot. If higher than the frequencies
-            in the PSD, all frequencies will be plotted.
-        ax : matplotlib axes, optional
-            Axes in which to plot the figure. If none provided, a new figure is
-            generated.
-        plot_chan : int, optional
-            Channel to plot (counting from 1). The default is None, in which 
-            case all channels' PSDs are plotted.
-        figsize : tuple, optional
-            Figure size in inches (only used if a new figure is created). The 
-            default is (5,5).
-        lw : float, optional
-            Linewidth of each PSD's line plot. The default is 0.5.
-
-        Returns
-        -------
-        fig : matplotlib figure
-            Figure handle.
-        ax : matplotlib axes
-            Axis handle.
-
-        '''
-        
-        # Check validity of start and stop frequencies
-        assert start_freq < stop_freq, (
-            'The first frequency to plot, start_freq, must be less than '
-            'the final frequency to plot, stop_freq'
-            )
-        
-        # Check validity of channel to plot
-        if plot_chan:
-            assert plot_chan <= self.n_chan, (
-                f'Cannot plot channel {plot_chan}: the EMG data only contains '
-                f'{self.n_chan} channels'
-                )
-        
-        # create new figure with specified size if no axis provided
-        if ax is None:
-            fig, ax = plt.subplots(figsize=figsize)
-        else:
-            fig = None
-
-        # Find frequencies to plot
-        plot_bool = np.all([self.freq >= start_freq, 
-                            self.freq <= stop_freq], 
-                           axis=0)
-        
-        # Plot all channels or specified channel
-        if plot_chan is None:
-            for i in range(self.n_chan):
-                ax.plot(self.freq[plot_bool], self.pxx[i,plot_bool],
-                        lw=lw)
-        else:
-            ax.plot(self.freq[plot_bool], self.pxx[plot_chan-1,plot_bool],
-                    lw=lw)
-            ax.set_title(self.chan.chan_names[plot_chan-1])
-        
-        # Axis labels
-        ax.set_xlabel('Frequency (Hz)')
-        ax.set_ylabel('PSD')
-        
-        return fig, ax
         
     
