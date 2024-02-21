4a49d848ca5f36fa2d2d62f88e0b88da1ff5ae9e
Gabrielle M. Schroeder <8205451+gmschroe@users.noreply.github.com>
2024-02-21 11:48:27 +0000

    Class to provide interface for preprocessing settings (#21)

---
+++ analysis_software/microemggui/microemggui/models/settings.py
@@ -0,0 +1,37 @@
+"""
+Models for settings for EMG data analysis.
+
+Note: does not use Qt classes for model/view framework. Purpose is to provide an
+interface to pymicroemg data classes for settings:
+    - EMGPreprocSettings
+"""
+
+import json
+
+from pymicroemg.emg_preproc_settings import EMGPreprocSettings
+
+
+class EMGPreprocSettingsModel:
+    # TODO: remove print statements or change to logging output
+
+    def __init__(self, settings: EMGPreprocSettings):
+        self.settings = settings
+
+    def mains_checkbox_toggled(self, checked):
+        # Slot for mains removal checkbox
+
+        print(f"MAINS REMOVAL: Is checkbox checked? : {checked}")
+        self.settings.remove_mains = checked
+        print(f"updated mains setting: {self.settings.remove_mains}")
+
+    def filter_checkbox_toggled(self, checked):
+        # Slot for filter checkbox
+
+        print(f"FILTER: Is checkbox checked? : {checked}")
+        self.settings.butterworth_filter = checked
+        print(f"updated filter setting: {self.settings.butterworth_filter}")
+
+    def print_settings(self):
+        # Print settings - currently used to verify data updates
+        print(json.dumps(vars(self.settings), indent=4))
+        print("\n")
