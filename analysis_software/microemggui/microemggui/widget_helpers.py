106c3bdd91291cb65ef6b4a38fe3363707335255
Gabrielle M. Schroeder <8205451+gmschroe@users.noreply.github.com>
2024-03-05 13:44:40 +0000

    removed defunct widget code - formatting is now performed using Qt style sheet

--- analysis_software/microemggui/microemggui/widget_helpers.py
+++ analysis_software/microemggui/microemggui/widget_helpers.py
@@ -1,17 +1,3 @@
-from PySide6.QtWidgets import QSizePolicy
-
-
-def fix_widget_size(my_widget, w, h):
-    # Set widget min size and set size policy to Fixed
-
-    # set size properties
-    w_sz = my_widget.sizeHint()
-    if w is not None:
-        w_sz.setWidth(w)
-    if h is not None:
-        w_sz.setHeight(h)
-    my_widget.setMinimumSize(w_sz)
-    my_widget.setMaximumSize(w_sz)
-
-    # size policy = fixed so does not expand if widget size changes
-    my_widget.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+"""
+Generic functions for creating and modifying widgets.
+"""
