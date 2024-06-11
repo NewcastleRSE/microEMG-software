39b36dc2604d3d122024b46721d028bd75273e14
Dr Richard Howey <43878787+drhowey@users.noreply.github.com>
2024-06-11 09:46:48 +0100

    Delete analysis_software/analysis directory
    Deleted directory to avoid merge conflicts with #41 
    Code is now in pymicroemg directory

--- analysis_software/analysis/emg_analyser_python/constants.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Constants to be used for the MicroEMG code
-"""
-
-# Use Python package for find peaks and quicker clustering approach
-QUICK_VERSION = False
