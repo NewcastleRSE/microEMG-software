b8787a16c4865509bf006a4b426489ef82190682
Gabrielle Schroeder <8205451+gmschroe@users.noreply.github.com>
2024-06-20 10:30:43 +0100

    refactored main window code - moved toolbars to separate file in main module (#73, #57)

--- analysis_software/microemggui/microemggui/widgets/main/main_window.py
+++ analysis_software/microemggui/microemggui/widgets/main/main_window.py
@@ -5,117 +5,28 @@ Widget for main window with toolbars and other navigation elements.
 
 """
 
-import os
-
 from palettable.cartocolors.qualitative import Prism_10
 
 from PySide6.QtWidgets import (
     QMainWindow,
     QWidget,
-    QToolBar,
-    QLabel,
-    QPushButton,
-    QButtonGroup,
     QVBoxLayout,
     QStackedLayout,
-    QSizePolicy,
 )
-from PySide6.QtCore import Qt, QSize
-from PySide6.QtGui import QAction, QIcon
+from PySide6.QtCore import Qt
 
 from microemggui.widgets.base import (
-    AnalysisToolbarButton,
-    AnalysisToolbarLabel,
     SectionTitle,
     ExpandingVSpacer,
 )
+
+from microemggui.widgets.main.toolbars import AnalysisToolbar, TopToolbar
 from microemggui.widgets.load.load_step import LoadWidget
 from microemggui.widgets.preproc.preproc_step import PreprocWidget
 from microemggui.models.settings import EMGPreprocSettingsModel
 
-# --- Widgets for main window ---
 
-
-class MicroEMGLogo(QPushButton):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Button icon
-        # TODO: set resource path or otherwise define path for icons
-        icon_dir = os.path.join(
-            "analysis_software",
-            "microemggui",
-            "microemggui",
-            "icons",
-            "bootstrap-icons-1.11.3",
-        )
-        logo_icon = "activity.svg"
-
-        self.setIcon(QIcon(os.path.join(icon_dir, logo_icon)))
-        self.setStatusTip("Home")
-        self.setCheckable(True)
-
-
-class AnalysisToolbar(QToolBar):
-    # Toolbar on left of window for navigating analysis steps
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Info about widgets to add to toolbar
-        toolbar_w_text = {
-            "preptext": "Prepare EMG",
-            "load": "1. Load",
-            "preprocess": "2. Preprocess",
-            "remove": "3. Remove channels",
-            "analysetext": "Analyse EMG",
-            "motorunits": "4. Find motor units",
-            "fibres": "5. Localise fibres",
-            "jitter": "6. Compute jitter",
-            "exporttext": "Export results",
-            "export": "Export",
-        }
-
-        toolbar_w_is_button = [
-            False,
-            True,
-            True,
-            True,
-            False,
-            True,
-            True,
-            True,
-            False,
-            True,
-        ]
-
-        # Create and add widgets
-        self.widgets = {}
-        button_group = QButtonGroup(self)  # group so can only click one at a time
-
-        # First add logo button for home page
-        self.widgets["home"] = MicroEMGLogo(parent=self)
-        self.addWidget(self.widgets["home"])
-        button_group.addButton(self.widgets["home"])
-
-        # Add analysis step buttons and section labels
-        for (w_name, text), button in zip(toolbar_w_text.items(), toolbar_w_is_button):
-            if button:  # Create buttons
-                self.widgets[w_name] = AnalysisToolbarButton(text, parent=self)
-                self.widgets[w_name].setEnabled(False)  # Disable buttons at start
-                self.widgets[w_name].setCheckable(True)  # Add checked state
-                button_group.addButton(self.widgets[w_name])  # Add to button group
-            else:  # Create section labels
-                self.widgets[w_name] = AnalysisToolbarLabel(text, parent=self)
-            self.addWidget(self.widgets[w_name])  # Add widget to toolbar
-
-        # Initial button states
-        self.widgets["load"].setEnabled(True)  # Enable first step (loading)
-        self.widgets["home"].toggle()
-
-        # Toolbar properties
-        self.setMovable(False)
-        self.setOrientation(Qt.Vertical)
+# --- Widgets to put within main window ---
 
 
 class WelcomeWidget(QWidget):
@@ -134,46 +45,6 @@ class WelcomeWidget(QWidget):
         layout.addItem(ExpandingVSpacer())  # spacer
         layout.setContentsMargins(20, 0, 0, 0)
         self.setLayout(layout)
-
-
-class TopToolbar(QToolBar):
-    # Toolbar on top of page for settings, info, and help links
-    # Also has label for recording
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Add placehold label for recording
-        # TODO: replace with custom label class
-        self.widgets = {"recording": QLabel("Recording:", parent=self)}
-        size_policy = self.widgets["recording"].sizePolicy()
-        size_policy.setHorizontalPolicy(QSizePolicy.Expanding)
-        self.widgets["recording"].setSizePolicy(size_policy)
-
-        for w in self.widgets.values():
-            self.addWidget(w)
-
-        # Icons for actions
-        # TODO: set resource path or otherwise define path for icons
-        icon_dir = os.path.join(
-            "analysis_software",
-            "microemggui",
-            "microemggui",
-            "icons",
-            "bootstrap-icons-1.11.3",
-        )
-        icons = ["gear.svg", "question-circle.svg", "info-circle.svg"]
-
-        tips = ["Settings", "Help", "About"]
-
-        for icon, tip in zip(icons, tips):
-            action = QAction(QIcon(os.path.join(icon_dir, icon)), tip, self)
-            action.setStatusTip(tip)
-            # TODO: create and connect to pop-up windows
-            self.addAction(action)
-
-        # Properties
-        self.setIconSize(QSize(16, 16))
 
 
 class AnalysisStepsWidget(QWidget):
---
+++ analysis_software/microemggui/microemggui/widgets/main/toolbars.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Main window toolbars and widgets placed in toolbars (e.g., logo button)
+"""
+
+import os
+
+from PySide6.QtWidgets import (
+    QToolBar,
+    QLabel,
+    QPushButton,
+    QButtonGroup,
+    QSizePolicy,
+)
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QAction, QIcon
+
+from microemggui.widgets.base import (
+    AnalysisToolbarButton,
+    AnalysisToolbarLabel,
+)
+
+# --- Widgets in toolbars ---
+
+
+class MicroEMGLogo(QPushButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Button icon
+        # TODO: set resource path or otherwise define path for icons
+        icon_dir = os.path.join(
+            "analysis_software",
+            "microemggui",
+            "microemggui",
+            "icons",
+            "bootstrap-icons-1.11.3",
+        )
+        logo_icon = "activity.svg"
+
+        self.setIcon(QIcon(os.path.join(icon_dir, logo_icon)))
+        self.setStatusTip("Home")
+        self.setCheckable(True)
+
+
+# --- Toolbars ---
+
+
+class AnalysisToolbar(QToolBar):
+    # Toolbar on left of window for navigating analysis steps
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Info about widgets to add to toolbar
+        toolbar_w_text = {
+            "preptext": "Prepare EMG",
+            "load": "1. Load",
+            "preprocess": "2. Preprocess",
+            "remove": "3. Remove channels",
+            "analysetext": "Analyse EMG",
+            "motorunits": "4. Find motor units",
+            "fibres": "5. Localise fibres",
+            "jitter": "6. Compute jitter",
+            "exporttext": "Export results",
+            "export": "Export",
+        }
+
+        toolbar_w_is_button = [
+            False,
+            True,
+            True,
+            True,
+            False,
+            True,
+            True,
+            True,
+            False,
+            True,
+        ]
+
+        # Create and add widgets
+        self.widgets = {}
+        button_group = QButtonGroup(self)  # group so can only click one at a time
+
+        # First add logo button for home page
+        self.widgets["home"] = MicroEMGLogo(parent=self)
+        self.addWidget(self.widgets["home"])
+        button_group.addButton(self.widgets["home"])
+
+        # Add analysis step buttons and section labels
+        for (w_name, text), button in zip(toolbar_w_text.items(), toolbar_w_is_button):
+            if button:  # Create buttons
+                self.widgets[w_name] = AnalysisToolbarButton(text, parent=self)
+                self.widgets[w_name].setEnabled(False)  # Disable buttons at start
+                self.widgets[w_name].setCheckable(True)  # Add checked state
+                button_group.addButton(self.widgets[w_name])  # Add to button group
+            else:  # Create section labels
+                self.widgets[w_name] = AnalysisToolbarLabel(text, parent=self)
+            self.addWidget(self.widgets[w_name])  # Add widget to toolbar
+
+        # Initial button states
+        self.widgets["load"].setEnabled(True)  # Enable first step (loading)
+        self.widgets["home"].toggle()
+
+        # Toolbar properties
+        self.setMovable(False)
+        self.setOrientation(Qt.Vertical)
+
+
+class TopToolbar(QToolBar):
+    # Toolbar on top of page for settings, info, and help links
+    # Also has label for recording
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Add placehold label for recording
+        # TODO: replace with custom label class
+        self.widgets = {"recording": QLabel("Recording:", parent=self)}
+        size_policy = self.widgets["recording"].sizePolicy()
+        size_policy.setHorizontalPolicy(QSizePolicy.Expanding)
+        self.widgets["recording"].setSizePolicy(size_policy)
+
+        for w in self.widgets.values():
+            self.addWidget(w)
+
+        # Icons for actions
+        # TODO: set resource path or otherwise define path for icons
+        icon_dir = os.path.join(
+            "analysis_software",
+            "microemggui",
+            "microemggui",
+            "icons",
+            "bootstrap-icons-1.11.3",
+        )
+        icons = ["gear.svg", "question-circle.svg", "info-circle.svg"]
+
+        tips = ["Settings", "Help", "About"]
+
+        for icon, tip in zip(icons, tips):
+            action = QAction(QIcon(os.path.join(icon_dir, icon)), tip, self)
+            action.setStatusTip(tip)
+            # TODO: create and connect to pop-up windows
+            self.addAction(action)
+
+        # Properties
+        self.setIconSize(QSize(16, 16))
