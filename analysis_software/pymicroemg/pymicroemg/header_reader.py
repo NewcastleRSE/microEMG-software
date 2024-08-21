32913f606d3cbe76915b969b9275bf8dc987c0eb
Fehings <frances.hutchings@ncl.ac.uk>
2024-08-21 15:32:26 +0100

    header reader line shortenings for linter

--- analysis_software/pymicroemg/pymicroemg/header_reader.py
+++ analysis_software/pymicroemg/pymicroemg/header_reader.py
@@ -1,4 +1,5 @@
-# This script is for a callable function designed to take a binary intan header file as an input
+# This script is for a callable function designed to take a binary intan header file as
+# an input
 # It will return the header identifier as a check to ensure it is parsing correctly
 # It will return the main version and secondary version numbers
 # It will return the sample rate
@@ -6,7 +7,6 @@
 # It will also check that the header ID is as expected and throw a warning if it is not.
 #
 # FT 2024
-
 # Library imports
 import numpy as np
 
@@ -64,11 +64,11 @@ def header_reader(header_file):
     if header_file.lower().endswith(".rhd"):
         pass
     else:
-        print(
-            "WARNING: File path does not appear to have an rhd extension, have you selected the correct file?"
-        )
-    # Not throwing an error here as I am unsure whether there are occasianally valid header files with a different extension
-    # and if they are invalid then this will be picked up with the header check or with a failed file read.
+        print("WARNING: File extension not .rhd, have you selected the correct file?")
+    # Not throwing an error here as I am unsure whether there are occasianally valid
+    # header files with a different extension
+    # and if they are invalid then this will be picked up with the header check or with
+    # a failed file read.
 
     # set the data type as per our expectations of the header file:
     dt = np.dtype(
@@ -80,7 +80,8 @@ def header_reader(header_file):
         ]
     )
 
-    # call numpy's fromfile with the above data type, for one instance of this datatype (i.e. first set of values that can be read as this dtype?)
+    # call numpy's fromfile with the above data type, for one instance of this datatype
+    # (i.e. first set of values that can be read as this dtype?)
     header = np.fromfile(header_file, dt, count=1)
 
     # check header ID
