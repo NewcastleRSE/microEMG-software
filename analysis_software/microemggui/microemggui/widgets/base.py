13c865fc574196f8d37831fc3e13599ccde925ac
Gabrielle Schroeder <8205451+gmschroe@users.noreply.github.com>
2024-09-11 10:12:08 +0100

    docstring updates for base classes, added license for pyqtgraph modified code (#96)

--- analysis_software/microemggui/microemggui/widgets/base.py
+++ analysis_software/microemggui/microemggui/widgets/base.py
@@ -24,105 +24,154 @@ from matplotlib.backends.backend_qtagg import NavigationToolbar2QT
 
 
 class CheckBoxMain(QCheckBox):
-    # Checkbox, main text (start of settings section)
+    """
+    Checkbox that also marks the start of a settings section.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class CheckBoxChannel(QCheckBox):
-    # Checkbox for channel
+    """
+    Checkbox for selecting EMG channel.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class CheckBoxRegular(QCheckBox):
-    # Radio button, regular text
+    """
+    Checkbox that is not emphasised (not a section heading)
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class InputComboBox(QComboBox):
-    # Combobox for settings input
+    """
+    Combobox for settings input.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class InputSpinBox(QSpinBox):
-    # Spinbox for settings input
+    """
+    Spinbox for settings input.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class InputLineEdit(QLineEdit):
-    # LineEdit for settings input
+    """
+    Line edit (text field) for settings input.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 # --- Labels ---
-# TODO: consider changing input text labels to more generic names (usable for more than input)
+# Note that "input" labels/text are also used more generally for messages that are not
+# associated with specified inputs.
 
 
 class InputLabel(QLabel):
-    # Label for input field (e.g., combobox)
+    """
+    Label for settings input field (e.g., combobox) that goes above the input widget.
+    Has emphasised text.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class InputInlineLabel(QLabel):
-    # Inline label for input field
+    """
+    Inline label for settings input field that is inline with the input widget.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class InputInlineText(QLabel):
-    # Text that is inline with other widget, but not label for widget (e.g., units)
+    """
+    Text that is inline with another widget, but is not label for widget (e.g., units).
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class InputInlineHighlightedText(QLabel):
-    # Text that is inline with other widget, but not label for widget (e.g., units)
-    # Highlighted in a different colour to make more prominent
+    """
+    Text that is inline with other widget, but not label for widget (e.g., units).
+    Highlighted in the accent colour in the style sheet to make more prominent.
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class InputWarningLabel(QLabel):
-    # Label for warning/error text for input fields
+    """
+    Label for warning/error text for input fields or other GUI messages.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setWordWrap(True)
 
 
 class InputExplanationLabel(QLabel):
-    # Explanation text for settings input
+    """
+    Explanation text for settings input.
+    Lighter text colour.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setWordWrap(True)
 
 
 class SubsectionTitle(QLabel):
-    # Label for subsection of a larger widget (e.g., settings)
+    """
+    Label for subsection of a larger widget (e.g., settings).
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class SectionTitle(QLabel):
-    # Label for a larger widget (e.g., preprocessing step)
+    """
+    Label for a page (widget) in the GUI (e.g., preprocessing step).
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class AnalysisToolbarLabel(QLabel):
-    # Labels for sections in analysis toolbar
+    """
+    Labels for sections in analysis toolbar.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class HighlightedLabel(QLabel):
-    # Label highlighted in a different colour to make more prominent
+    """
+    Label highlighted in a different colour to make more prominent.
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -150,32 +199,45 @@ class TitleInputLabel(QLabel):
 
 
 class SmallPushButton(QPushButton):
-    # Small push buttons
+    """
+    Small push buttons (i.e., secondary buttons).
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class LargePushButton(QPushButton):
-    # Large push buttons
+    """
+    Large push buttons (i.e., primary buttons).
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class AnalysisToolbarButton(QPushButton):
-    # Push buttons for analysis toolbar
+    """
+    Push buttons for the analysis toolbar.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class WidgetControlButton(QToolButton):
-    # Button for controlling widget (e.g., EMG viewer plot settings)
+    """
+    Small tool buttons for controlling a widget (e.g., the EMG viewer plot settings).
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class MotorUnitButton(QPushButton):
-    # Button for motor units in selectmu widget
+    """
+    Button for motor units in the select motor units (selectmu) widget.
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -185,34 +247,47 @@ class MotorUnitButton(QPushButton):
 
 
 class ExpandingSpacer(QSpacerItem):
-    # Spacer with minimum size of (0, 0) that will expand to fill available space in
-    # widget.
-    # Used to keep other widgets a fixed size.
+    """
+    Spacer with minimum size of (0, 0) that will expand to fill available space in
+    widget.
+    Used to keep other widgets a fixed size.
+    """
+
     def __init__(self):
         super().__init__(0, 0, QSizePolicy.Expanding, QSizePolicy.Expanding)
 
 
 class ExpandingVSpacer(QSpacerItem):
-    # Spacer with minimum size of (0, 0) that will expand vertically to fill available
-    # space in widget.
-    # Used to keep other widgets a fixed size.
+    """
+    Spacer with minimum size of (0, 0) that will expand vertically to fill available
+    space in widget.
+    Used to keep other widgets a fixed size.
+    """
+
     def __init__(self):
         super().__init__(0, 0, QSizePolicy.Fixed, QSizePolicy.Expanding)
 
 
 class ExpandingHSpacer(QSpacerItem):
-    # Spacer with minimum size of (0, 0) that will expand horizontally to fill available
-    # space in widget.
-    # Used to keep other widgets a fixed size.
+    """
+    Spacer with minimum size of (0, 0) that will expand horizontally to fill available
+    space in widget.
+    Used to keep other widgets a fixed size.
+    """
+
     def __init__(self):
         super().__init__(0, 0, QSizePolicy.Expanding, QSizePolicy.Fixed)
 
 
-# --- Matplotlib widgets ---
+# --- Misc widgets ---
 
 
 class MatplotlibToolbar(NavigationToolbar2QT):
+    """
+    Toolbar for controlling/saving matplotlib plots.
+    This child class decreases the size of the icons.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setIconSize(QSize(20, 20))
-        # self.setStyleSheet("background-color:white;")
--- analysis_software/microemggui/microemggui/widgets/base_pyqtgraph.py
+++ analysis_software/microemggui/microemggui/widgets/base_pyqtgraph.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 16 16:16:00 2024
+Modification of pyqtgraph classes to provide additional plotting options.
 
-@author: Gabrielle
+Modifies code from pyqtgraph
+https://github.com/pyqtgraph/pyqtgraph/blob/
+02482118572e76b4997761dcc5f652c3335247f3/pyqtgraph/graphicsItems/AxisItem.py#L1254-L1281
+which is licensed under the MIT License:
+
+LICENSE
+
+Copyright (c) 2012  University of North Carolina at Chapel Hill
+Luke Campagnola    ('luke.campagnola@%s.com' % 'gmail')
+
+The MIT License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
 """
 from pyqtgraph import AxisItem
 
